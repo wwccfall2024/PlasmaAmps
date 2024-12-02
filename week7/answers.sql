707df342dfd07d2e8259b9dace9c1c38a63f9cc4
PlasmaAmps <155706166+PlasmaAmps@users.noreply.github.com>
2024-12-02 09:38:00 -0800

    Added more tables to answers.sql

--- week7/answers.sql
+++ week7/answers.sql
@@ -8,3 +8,80 @@ CREATE TABLE players (
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE characters (
+  character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  player_id INT UNSIGNED,
+  name VARCHAR(30) NOT NULL,
+  level INT UNSIGNED NOT NULL,
+  FOREIGN KEY (player_id) REFERENCES players(player_id) 
+    ON DELETE SET NULL 
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE winners (
+  character_id INT UNSIGNED PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  FOREIGN KEY (character_id) REFERENCES characters(character_id)
+    ON DELETE SET NULL 
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE character_stats (
+  character_id INT UNSIGNED PRIMARY KEY NOT NULL,
+  health INT UNSIGNED NOT NULL,
+  armor INT UNSIGNED NOT NULL,
+  FOREIGN KEY (character_id) REFERENCES characters(character_id)
+    ON DELETE SET NULL 
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE teams (
+  team_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30)
+);
+
+CREATE TABLE team_members (
+  team_member_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  team_id INT UNSIGNED NOT NULL,
+  character_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (team_id) REFERENCES teams(team_id)
+    ON DELETE SET NULL 
+    ON UPDATE CASCADE,
+  FOREIGN KEY (character_id) REFERENCES characters(character_id)
+    ON DELETE SET NULL 
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE items (
+  item_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  armor INT UNSIGNED NOT NULL,
+  damage INT UNSIGNED NOT NULL
+);
+
+CREATE TABLE inventory (
+  inventory_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  character_id INT UNSIGNED NOT NULL,
+  item_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (character_id) REFERENCES characters(character_id)
+    ON DELETE SET NULL 
+    ON UPDATE CASCADE,
+  FOREIGN KEY (item_id) REFERENCES items(item_id)
+    ON DELETE SET NULL 
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE equipped (
+  equipped_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  character_id INT UNSIGNED NOT NULL,
+  item_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (character_id) REFERENCES characters(character_id)
+    ON DELETE SET NULL 
+    ON UPDATE CASCADE,
+  FOREIGN KEY (item_id) REFERENCES items(item_id)
+    ON DELETE SET NULL 
+    ON UPDATE CASCADE
+);
+
+
