61e18ca22f67a89291ce8427458ed5f6ec67f8c2
PlasmaAmps <155706166+PlasmaAmps@users.noreply.github.com>
2024-12-09 14:31:00 -0800

    added two tables
    added 2 of the tables, this is mainly just a quick save as I need to do something and don't want to lose the progress

--- week9/answers.sql
+++ week9/answers.sql
@@ -1,3 +1,21 @@
 -- Create your tables, views, functions and procedures here!
 CREATE SCHEMA social;
 USE social;
+
+CREATE TABLE users (
+  user_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  created_on DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE sessions (
+  session_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  user_id INT UNSIGNED,
+  created_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+);
