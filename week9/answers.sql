888e1d37002e6ae529b29d0b7fe5e2b90a59a0d7
PlasmaAmps <155706166+PlasmaAmps@users.noreply.github.com>
2024-12-13 11:28:54 -0800

    Reverted because it caused to many things to fail when I tried to better the code  ):

--- week9/answers.sql
+++ week9/answers.sql
@@ -91,39 +91,38 @@ CREATE TRIGGER after_user_insert
 AFTER INSERT ON users
 FOR EACH ROW
 BEGIN
-    -- Declare the variables
-    DECLARE new_post_id INT;
-    DECLARE user_id INT;
     DECLARE done INT DEFAULT 0;
-    DECLARE user_cursor CURSOR FOR 
-        SELECT user_id 
-        FROM users 
-        WHERE user_id != NEW.user_id;
+    DECLARE cur_user_id INT;
 
-    -- Declare a handler to deal with the end of the cursor
+    -- Declare a cursor for selecting all user_ids except the new user
+    DECLARE user_cursor CURSOR FOR 
+    SELECT user_id 
+    FROM users 
+    WHERE user_id != NEW.user_id;
+
+    -- Declare a handler for the end of the cursor
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
-    -- Insert a new post for the new user
+    -- Create a new post for the new user
     INSERT INTO posts (user_id, content, created_on, updated_on)
     VALUES (NEW.user_id, CONCAT(NEW.first_name, ' ', NEW.last_name, ' just joined!'), NOW(), NOW());
 
-    -- Get the post_id of the newly created post
-    SET new_post_id = LAST_INSERT_ID();
+    -- Get the newly created post_id
+    SET @new_post_id = LAST_INSERT_ID();
 
-    -- Open the cursor to loop through each user
+    -- Open the cursor
     OPEN user_cursor;
 
-    -- Fetch the first user_id from the cursor
-    FETCH user_cursor INTO user_id;
+    -- Fetch the first user_id into the cursor variable
+    FETCH user_cursor INTO cur_user_id;
 
-    -- Loop through the cursor to insert notifications for each user except the new one
+    -- Loop through each user_id
     WHILE done = 0 DO
         -- Insert a notification for the current user
-        INSERT INTO notifications (user_id, post_id)
-        VALUES (user_id, new_post_id);
+        INSERT INTO notifications (user_id, post_id) VALUES (cur_user_id, @new_post_id);
 
         -- Fetch the next user_id
-        FETCH user_cursor INTO user_id;
+        FETCH user_cursor INTO cur_user_id;
     END WHILE;
 
     -- Close the cursor
@@ -131,7 +130,6 @@ BEGIN
 END$$
 
 DELIMITER ;
-
 
 
 
@@ -143,50 +141,20 @@ DO
 
 
 DELIMITER $$
-
 CREATE PROCEDURE add_post(IN userId INT, IN postContent TEXT)
 BEGIN
-    -- Declare the variables
-    DECLARE new_post_id INT;
-    DECLARE friend_id INT;
-    DECLARE done INT DEFAULT 0;
-    DECLARE friend_cursor CURSOR FOR 
-        SELECT friend_id 
-        FROM friends 
-        WHERE user_id = userId;
-    
-    -- Declare a handler to deal with the end of the cursor
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-
     -- Insert the new post
     INSERT INTO posts (user_id, content, created_on, updated_on)
     VALUES (userId, postContent, NOW(), NOW());
 
     -- Get the post_id of the newly created post
-    SET new_post_id = LAST_INSERT_ID();
+    SET @new_post_id = LAST_INSERT_ID();
 
-    -- Open the cursor to loop through each friend
-    OPEN friend_cursor;
-
-    -- Fetch the first friend_id from the cursor
-    FETCH friend_cursor INTO friend_id;
-
-    -- Loop through the cursor to insert notifications for each friend
-    WHILE done = 0 DO
-        -- Insert a notification for the current friend
-        INSERT INTO notifications (user_id, post_id)
-        VALUES (friend_id, new_post_id);
-
-        -- Fetch the next friend_id
-        FETCH friend_cursor INTO friend_id;
-    END WHILE;
-
-    -- Close the cursor
-    CLOSE friend_cursor;
+    -- Notify all friends of the user
+    INSERT INTO notifications (user_id, post_id)
+    SELECT friend_id, @new_post_id
+    FROM friends
+    WHERE user_id = userId;
 END;
 $$
-
 DELIMITER ;
-
-
-
